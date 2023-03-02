6195e888ef8530b9a1a954abb32801a59e22e9e1
Ruslan Shevchenko <ruslan91999@gmail.com>
2023-03-02 15:48:37 +0300

    feat: Добавит props в основной компонент Main.tsx

--- project/src/components/small-film-card/SmallFilmCard.tsx
+++ project/src/components/small-film-card/SmallFilmCard.tsx
@@ -5,11 +5,10 @@ function SmallFilmCard(): JSX.Element {
         <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175"/>
       </div>
       <h3 className="small-film-card__title">
-        <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of
-          Grindelwald</a>
+        <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
       </h3>
     </article>
-  )
+  );
 }
 
 export default SmallFilmCard;
