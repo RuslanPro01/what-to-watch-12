3a9bbb8a962cb83f1bf2e84e6e0690632205d67c
RuslanPro01 <ruslan91999@gmail.com>
2023-04-10 14:22:38 +0300

    refactor: Вынесет кноаки показа карточек в отдельный компонент

---
+++ project/src/components/main-catalog-films/catalog-more-button.tsx
@@ -0,0 +1,7 @@
+export function CatalogMoreButton(): JSX.Element | null {
+  return (
+    <div className="catalog__more">
+      <button className="catalog__button" type="button">Show more</button>
+    </div>
+  );
+}
