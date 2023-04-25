17f9dea4d30e67e1e6b0a3e32d0ea5b70817cb9f
RuslanPro01 <ruslan91999@gmail.com>
2023-04-26 02:08:30 +0300

    refactor: Исправит нейминг

--- project/src/components/reviews/reviews.tsx
+++ project/src/components/reviews/reviews.tsx
@@ -8,11 +8,11 @@ type ReviewsProps = {
 export function Reviews({comments}: ReviewsProps): JSX.Element {
   const divisionNumber = Math.round(comments.length / 2);
   const leftColumnReviews = comments.slice(0, divisionNumber);
-  const RightColumnReviews = comments.slice(divisionNumber);
+  const rightColumnReviews = comments.slice(divisionNumber);
   return (
     <>
       <ColumnReviews comments={leftColumnReviews}/>
-      {comments.length > 1 ? <ColumnReviews comments={RightColumnReviews}/> : null}
+      {comments.length > 1 ? <ColumnReviews comments={rightColumnReviews}/> : null}
     </>
   );
 }
